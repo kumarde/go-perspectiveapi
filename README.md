5699855e590e0fe8865ca590a81b8ae0b7a5d619
Deepak Kumar <kumarde@umich.edu>
2018-09-06 16:18:47 -0500

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # go-perspectiveapi 
 go-perspectiveapi is a Go client library for [Google Jigsaw's Perspective
 API](https://www.perspectiveapi.com/)
+
+Currently, the AnalyzeComment endpoint is supported with default parameters.
+I plan to add features as necessary for my purposes - but of course, pull
+requests are welcome.
+
+See `main.go` for how to use the library in its current form.
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/teamnsrg/go-perspectiveapi/perspective"
 )
 
 func main() {
 	perspectiveClient := perspective.NewAnalyzeCommentClient(PERSPECTIVE_API_KEY)
-	perspectiveClient.AnalyzeComment("I hate you.")
+	analyzeComment := perspectiveClient.AnalyzeComment("toss my salad")
+	resp, _ := json.Marshal(analyzeComment)
+	fmt.Println(string(resp))
 }
